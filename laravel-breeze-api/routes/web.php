13909f886bda83db21f342c8a7c2bd192d5fc7e0
TAlexa1997 <alejandra.marianella.8@gmail.com>
2024-02-28 18:26:03 +0100

    react létrehozása

--- laravel-breeze-api/routes/web.php
+++ laravel-breeze-api/routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::get('/token', function () {
+    return request()->session()->token();
+});
+
 require __DIR__.'/auth.php';
